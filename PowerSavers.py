6ec1e3d82293f75178b2f7a7ec9a9d387ead0f24
rajaramvijayjax <142600244+rajaramvijayjax@users.noreply.github.com>
2023-08-20 09:57:01 -0400

    Add files via upload

--- PowerSavers.py
+++ PowerSavers.py
@@ -15,8 +15,8 @@ def kill_non_essential_processes():
     essential_processes = [
         "System", "svchost.exe", "explorer.exe", "winlogon.exe",
         "csrss.exe", "lsass.exe", "spoolsv.exe", "services.exe",
-        "smss.exe", "conhost.exe", "wininit.exe", "pythonw.exe",
-        "python.exe"
+        "smss.exe", "conhost.exe", "wininit.exe", "pythonw.exe","python.exe",
+        "python.exe", "py.exe"
     ]
 
     terminated_processes = []
